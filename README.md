8ce00c52a1e58e7decc06f14bfd286bf05fe7b01
Coenraad Stijne <coenraad.stijne@gmail.com>
2017-11-16 20:47:37 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Copy WSL Path README
+
+This extension adds 'Copy WSL Path' to the explorer's context menu.
+
+![Context Menu](/images/context-menu.png)
+
+## Requirements
+
+It uses the copy-paste NPM module, which has the following requirements:
+- pbcopy/pbpaste (for OSX)
+- xclip (for Linux and OpenBSD)
+- clip (for Windows). 
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Copy WSL Path
+
---
+++ src/extension.ts
@@ -0,0 +1,19 @@
+'use strict';
+import * as ncp from "copy-paste";
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    let disposable = vscode.commands.registerCommand('copy-WSL-path.copy', (e) => {
+        const text = "/mnt" + (e.path as string).replace(':', '');
+        ncp.copy(text);
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
